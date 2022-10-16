8ef10b3840e97a25e232c584b46557454c9a4d7c
Hwang <hku2002@hanmail.net>
2022-10-16 13:57:03 +0900

    feat: 주문 완료, 주문 취소 메소드 추가

--- src/main/java/com/commerce/order/controller/OrderController.java
+++ src/main/java/com/commerce/order/controller/OrderController.java
@@ -5,8 +5,7 @@ import com.commerce.global.common.dto.PagingCommonRequestDto;
 import com.commerce.order.service.OrderServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +16,17 @@ public class OrderController {
     @GetMapping("/v1/orders")
     public ResponseEntity<?> orders(PagingCommonRequestDto pagingCommonRequestDto) {
         return CommonResponse.setResponse(orderServiceImpl.findOrders(1L, pagingCommonRequestDto.getLimit(), pagingCommonRequestDto.getOffset()));
+    }
+
+    @PostMapping("/v1/order")
+    public ResponseEntity<?> addOrder() {
+        orderServiceImpl.addOrder();
+        return CommonResponse.setResponse();
+    }
+
+    @PutMapping("/v1/order/{orderId}")
+    public ResponseEntity<?> cancelOrder(@PathVariable Long orderId) {
+        orderServiceImpl.cancelOrder();
+        return CommonResponse.setResponse();
     }
 }
--- src/main/java/com/commerce/order/service/OrderServiceImpl.java
+++ src/main/java/com/commerce/order/service/OrderServiceImpl.java
@@ -5,12 +5,14 @@ import com.commerce.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OrderServiceImpl {
 
     private final OrderRepository orderRepository;
@@ -22,6 +24,19 @@ public class OrderServiceImpl {
     public List<OrderResponseDto> findOrders(Long userId, int limit, int offset) {
         return orderRepository.findAllWithOrderProducts(userId, PageRequest.of(limit, offset))
                 .stream().map(OrderResponseDto::new).collect(Collectors.toList());
+    }
+
+    /**
+     * 주문 추가 (주문 완료)
+     */
+    @Transactional
+    public void addOrder() {
+    }
+
+    /**
+     * 주문 취소
+     */
+    public void cancelOrder() {
     }
 
 }
