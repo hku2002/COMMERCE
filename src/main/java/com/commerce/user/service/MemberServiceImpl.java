3d451db2aa59196df4f857c52179f66ff69098c1
Hwang <hku2002@hanmail.net>
2022-11-12 22:44:35 +0900

    refactor: 미사용 메소드 제거 및 중복체크 함수 위치 변경
    - findOne 메소드 제거
    - 중복체크 메소드 Member class로 위치 변경

--- src/main/java/com/commerce/user/service/MemberServiceImpl.java
+++ src/main/java/com/commerce/user/service/MemberServiceImpl.java
@@ -1,13 +1,11 @@
 package com.commerce.user.service;
 
-import com.commerce.global.common.exception.BadRequestException;
 import com.commerce.user.domain.Member;
 import com.commerce.user.dto.JoinMemberDto;
 import com.commerce.user.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.ObjectUtils;
 
 
 @Service
@@ -23,29 +21,9 @@ public class MemberServiceImpl {
      */
     @Transactional
     public void join(JoinMemberDto joinMemberDto) {
-        checkMemberDuplicate(memberRepository.findByUserIdAndActivated(joinMemberDto.getUserId(), true));
+        Member.checkMemberDuplicate(memberRepository.findByUserIdAndActivated(joinMemberDto.getUserId(), true));
         joinMemberDto.encryptPassword(joinMemberDto.getPassword());
         memberRepository.save(joinMemberDto.toEntity()).getId();
-    }
-
-    /**
-     * 회원 조회
-     * param userId 회원 아이디
-     */
-    public Member findOne(String userId) {
-        Member member = memberRepository.findByUserIdAndActivated(userId, true);
-        member.checkMemberExist(member);
-        return member;
-    }
-
-    /**
-     * 회원 중복 체크
-     * @param member 회원 객체
-     */
-    private void checkMemberDuplicate(Member member) {
-        if (!ObjectUtils.isEmpty(member)) {
-            throw new BadRequestException("이미 가입된 회원이 존재합니다.");
-        }
     }
 
 }
