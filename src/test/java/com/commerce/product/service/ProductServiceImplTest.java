fe93be6610d25bec93abb7fef454549da2b1a501
Hwang <hku2002@hanmail.net>
2022-11-12 23:01:55 +0900

    refactor: mock 활용 방식으로 변경

--- src/test/java/com/commerce/product/service/ProductServiceImplTest.java
+++ src/test/java/com/commerce/product/service/ProductServiceImplTest.java
@@ -1,73 +1,36 @@
 package com.commerce.product.service;
 
-import com.commerce.global.common.Price;
-import com.commerce.global.common.dto.PagingCommonRequestDto;
-import com.commerce.product.domain.Product;
+import com.commerce.global.common.exception.BadRequestException;
 import com.commerce.product.repository.ProductRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Description;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 
-import static com.commerce.global.common.Price.DiscountMethod.PRICE;
-import static com.commerce.product.domain.Product.CompositionType.SINGLE;
-import static com.commerce.product.domain.Product.DisplayStatus.DISPLAY;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class ProductServiceImplTest {
 
-    @Autowired
+    @Mock
     ProductRepository productRepository;
 
-    @Autowired
+    @InjectMocks
     ProductServiceImpl productServiceImpl;
 
     @Test
-    @Description("상품조회")
-    void findProductsTest() {
+    @DisplayName("상품상세 조회 시 상품 데이터가 없을 경우 예외를 던진다.")
+    void findProductEmptyProductThrow() {
         // given
-        Price price = Price.builder()
-                .defaultPrice(10000)
-                .discountPrice(1000)
-                .salePrice(9000)
-                .discountRate(0)
-                .discountMethod(PRICE)
-                .build();
-
-        List<Product> products = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            products.add(Product.builder()
-                    .imgPath("/img/test" + i)
-                    .price(price)
-                    .compositionType(SINGLE)
-                    .name("test 상품" + i)
-                    .status(DISPLAY)
-                    .mainItemId(1L)
-                    .build());
-        }
-
-        productRepository.saveAll(products);
-
-        // when
-        PagingCommonRequestDto pagingCommonRequestDto = new PagingCommonRequestDto();
-        pagingCommonRequestDto.setLimit(0);
-        pagingCommonRequestDto.setOffset(3);
+        given(productRepository.findByIdAndActivated(anyLong(), anyBoolean())).willReturn(null);
 
         // then
-        assertAll(
-                () -> assertThat(productServiceImpl.findProducts(pagingCommonRequestDto).get(0).getId(), is(1L)),
-                () -> assertThat(productServiceImpl.findProducts(pagingCommonRequestDto).get(1).getId(), is(2L)),
-                () -> assertThat(productServiceImpl.findProducts(pagingCommonRequestDto).get(2).getId(), is(3L))
-        );
-
+        assertThatThrownBy(() -> productServiceImpl.findProduct(1L)).isInstanceOf(BadRequestException.class);
     }
 
 }
